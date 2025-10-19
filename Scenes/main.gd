223033dbcd4969fca20dd35bbf48f55cb7d10038
Alex Giller <alexgiller@proton.me>
2025-10-19 16:18:00 -0400

    added final stage and end screen

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,19 +1,14 @@
 extends Node2D
 
-const stages=["res://Scenes/stages/stage_1.tscn","res://Scenes/stages/stage_2.tscn","res://Scenes/stages/shop.tscn","res://Scenes/stages/stage_3.tscn","res://Scenes/stages/stage_4.tscn"]
+const stages=["res://Scenes/stages/stage_1.tscn","res://Scenes/stages/stage_2.tscn","res://Scenes/stages/shop.tscn","res://Scenes/stages/stage_3.tscn","res://Scenes/stages/stage_4.tscn","res://Scenes/stages/shop.tscn","res://Scenes/stages/final_stage.tscn","res://Scenes/win_screen.tscn"]
 var stageNum:=-1
 @export var stage:CanvasItem
 var playerAnimationProgress:=0.0
 var playerAnimationStartPos:Vector2
 var totalStageTresures:=0
 var money:=0
-var showUI:=true
-
-#func _ready() -> void:
-	#loadStage(stages[0],true)
 
 func loadNextStage() -> void:
-	print('laoading next stage')
 	stageNum+=1
 	loadStage(stages[stageNum])
 
@@ -55,7 +50,6 @@ static func formatTime(time:float) -> String:
 	return str(floori(time/60)).pad_zeros(1)+":"+str(floori(time)%60).pad_zeros(2)+"."+str(floori(time*100)%100).pad_zeros(2)
 
 func _process(delta: float) -> void:
-	
 	if Input.is_action_just_pressed("debug_1"):
 		if stage is Stage:
 			loadNextStage()
@@ -64,7 +58,10 @@ func _process(delta: float) -> void:
 	
 	if stage is TresureStage:
 		%"Time Left".text=formatTime(stage.get_node("Time Left").time_left)
-		%Reward.text="+"+str(stage.get_reward())
+		if stageNum==6:
+			%Reward.text="WIN"
+		else:
+			%Reward.text="+"+str(stage.get_reward())
 	if $Player.animating and stage is TresureStage:
 		playerAnimationProgress+=delta
 		if playerAnimationProgress>=1:
@@ -78,10 +75,9 @@ func _process(delta: float) -> void:
 func collectedTresure(tresure:Area2D):
 	tresure.found()
 	$Camera2D.add_trauma(0.3)
-	print(len(stage.getTresures()))
 	%"Tresure Counter".text="Tresures: "+str(totalStageTresures-len(stage.getTresures()))+"/"+str(totalStageTresures)
 	if len(stage.getTresures())==0:
-		print('stage finished')
+		
 		money+=stage.get_reward()
 		%Money.text=str(money)
 		loadNextStage()
